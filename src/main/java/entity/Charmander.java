e315b38d9c08070b84aa5b8c5caa8955e4b80c6d
manuel-vl <manuel.valencia@mercadolibre.com.co>
2024-02-24 23:04:01 -0500

    [FEATURE] Add Class Pokemon and Interfaces

---
+++ src/main/java/entity/Charmander.java
@@ -0,0 +1,33 @@
+package entity;
+
+public class Charmander extends Pokemon implements IFire{
+    @Override
+    protected void attackTackle() {
+        System.out.println("Hola soy Charmander, ataque placaje");
+    }
+
+    @Override
+    protected void attackScratch() {
+        System.out.println("Hola soy Charmander, ataque aranazo");
+    }
+
+    @Override
+    protected void attackNibble() {
+        System.out.println("Hola soy Charmander, ataque mordisco");
+    }
+
+    @Override
+    public void attackPunioFuego() {
+        System.out.println("Hola soy Charmander, ataque punio fuego");
+    }
+
+    @Override
+    public void attackLanzallamas() {
+        System.out.println("Hola soy Charmander, ataque lanzallamas");
+    }
+
+    @Override
+    public void attackAscuas() {
+        System.out.println("Hola soy Charmander, ataque ascuas");
+    }
+}
